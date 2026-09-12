7afb4d1f7d936db9029e574b7ebe562d8bcb3c81
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-12 22:21:16 +0700

    bai 6-9

---
+++ Bai6-9.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai6_9
+    {
+        public int max(int a, int b, int c)
+        {
+            return Math.Max(a, Math.Max(b, c));
+        }
+        public bool kiemtrasnt(int n)
+        {
+            if(n < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+        public void hoanvi(ref double a, ref double b)
+        {
+            double temp = a;
+            a = b;
+            b = temp;
+        }
+        public void timminmax(double a, double b, double c, out double min, out double max)
+        {
+            min = Math.Min(a, Math.Min(b, c));
+            max = Math.Max(a, Math.Max(b, c));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,8 +12,30 @@ namespace THUCHANH01
             //bai2.Chaybai2();
             //Bai3 bai3 = new Bai3();
             //bai3.Chaybai3();
-            Bai5 b5 = new Bai5();
-            b5.Chaybai5();
+            //Bai5 b5 = new Bai5();
+            //b5.Chaybai5();
+            Bai6_9 baiTap = new Bai6_9();
+            Console.WriteLine("- Test bai 6 -");
+            int maxBai6 = baiTap.max(15, 8, 4);
+            Console.WriteLine($"So lon nhat la : {maxBai6}");
+
+            Console.WriteLine("- Test bai 7 -");
+            int soCanKiemTra = 29;
+            bool SNT = baiTap.kiemtrasnt(soCanKiemTra);
+            Console.WriteLine($"So {soCanKiemTra} co phai la so nguyen to khong ? : {SNT}");
+            Console.WriteLine("- Test bai 8 (ref) -");
+            double x = 5.5;
+            double y = 9.9;
+            Console.WriteLine($"Truoc hoan vi: x = {x}, y = {y}");
+            baiTap.hoanvi(ref x, ref y);
+            Console.WriteLine($"Sau hoan vi: x = {x}, y = {y}");
+            Console.WriteLine("- Test bai 9 (out) -");
+            double a = 3.3, b = 7.7, c = 1.1;
+            double min, max;
+            baiTap.timminmax(a, b, c, out min, out max);
+            Console.WriteLine($"Gia tri nho nhat la: {min}, Gia tri lon nhat la: {max}");
+
+
             Console.ReadLine();
         }
     }
