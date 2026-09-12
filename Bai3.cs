8446f42734d87594aa314a92ca58888b6feda9be
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-12 18:08:02 +0700

    bai 3,4

---
+++ Bai3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace THUCHANH01
+{
+    public class Bai3
+    {
+        public void Chaybai3()
+        {
+            Console.Write("Nhap so nguyen x: ");
+            string inputX = Console.ReadLine();
+            Console.Write("Nhap so nguyen y: ");
+            string inputY = Console.ReadLine();
+            bool isValidX = int.TryParse(inputX, out int x);
+            bool isValidY = int.TryParse(inputY, out int y);
+            if (isValidX && isValidY)
+            {
+                Console.WriteLine($"Ket qua {x} mu {y} la : {Math.Pow(x, y)}");
+            }
+            else
+            {
+                Console.WriteLine("Gia tri nhap khong phai so nguyen !");
+            }
+        }
+    }
+}
